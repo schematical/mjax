d04e6c12d727c938d09315a7b1b28d8b4e698bdf
Matt Lea <github@mattleaconsulting.com>
2013-04-06 12:03:55 -0500

    mjax

--- _core/MJaxFormBase.class.php
+++ _core/MJaxFormBase.class.php
@@ -138,26 +138,20 @@ class MJaxFormBase{
              //Create a proxy for the main content panel
              $objClass->pxyMainWindow = new MJaxControlProxy($objClass, 'mainWindow_inner');
              $objClass->Form_Create();
-			 foreach(self::$arrExtensions as $intIndex => $objExtension){
-				$objExtension->InitControl($objClass);
-			}
+			 
         }else{
             $objClass->strCallType = MJaxCallType::None;
 			 //Create a proxy for the main content panel
             $objClass->pxyMainWindow = new MJaxControlProxy($objClass, 'mainWindow_inner');
-            $objClass->Form_Create();
-            foreach(self::$arrExtensions as $intIndex => $objExtension){
-				
-				$objExtension->InitControl($objClass);
-			}
-           
-			
-			
+            $objClass->Form_Create();           
         }
 		try{
 			MJaxForm::DefineAssetDir($objClass->strAssetMode);
 		}catch(Exception $e){
 			error_log($e->getMessage());
+		}
+		foreach(self::$arrExtensions as $intIndex => $objExtension){
+			$objExtension->InitControl($objClass);
 		}
         if(key_exists(MJaxFormPostData::ACTION, $_POST)){
         	self::$objForm = $objClass;
@@ -178,7 +172,7 @@ class MJaxFormBase{
 	            $objClass->strTemplate = MJaxForm::LocateTemplate($_SERVER[MLCServer::SCRIPT_FILENAME], $objClass->strAssetMode);
 				if(!file_exists($objClass->strTemplate)){
 					if($objClass->strAssetMode == MJaxAssetMode::MOBILE){
-						$objClass->strTemplate  = __VIEW_CORE_DIR__ . '/' . $objClass->strAssetMode . '/form.tpl.php';
+						$objClass->strTemplate  = __MJAX_CORE_VIEW__ . '/' . $objClass->strAssetMode . '/form.tpl.php';
 					}
 				}
 			}
@@ -199,7 +193,7 @@ class MJaxFormBase{
     }
     public static function LocateTemplate($strFileLoc, $strAssetMode){
     	if(is_null(self::$strTemplateDirOverride)){
-    		$strTemplateDir = __VIEW_APPS_DIR__ .'/'. MLCApplicationBase::$strInitedApp . '/' . $strAssetMode;//__VIEW_MAIN_APP_DIR__;
+    		$strTemplateDir = __VIEW_ACTIVE_APP_DIR__ . '/' . $strAssetMode;//__VIEW_MAIN_APP_DIR__;
     	}else{
     		$strTemplateDir = MJaxForm::$strTemplateDirOverride;
     	}
@@ -207,15 +201,16 @@ class MJaxFormBase{
 		/*if(!is_null(MJaxForm::$strTemplateRemove)){
 			$strFileLoc = str_replace(MJaxForm::$strTemplateRemove, '', $strFileLoc);
 		}*/
+		
         $strFileName = str_replace('.class.php', '', $strFileLoc);
-        $strFileName = str_replace('.php', '', $strFileName);
-        $strFileName = str_replace(__CTL_DIR__, $strTemplateDir, $strFileName);
-        
+        $strFileName = str_replace('.php', '', $strFileName);		  
+        $strFileName = str_replace(__INSTALL_ROOT_DIR__, $strTemplateDir, $strFileName);
+      
         return $strFileName . '.tpl.php';
     }
 	public function RenderTemplate($strFileLoc){
 		if(is_null(self::$strTemplateDirOverride)){
-    		$strTemplateDir = __VIEW_APPS_DIR__ .'/'. MLCApplicationBase::$strInitedApp . '/' . $this->strAssetMode;//__VIEW_MAIN_APP_DIR__;
+    		$strTemplateDir = __VIEW_ACTIVE_APP_DIR__  . '/' . $this->strAssetMode;//__VIEW_MAIN_APP_DIR__;
     	}else{
     		$strTemplateDir = MJaxForm::$strTemplateDirOverride;
     	}
@@ -229,14 +224,14 @@ class MJaxFormBase{
 
     protected function Render(){
     	if($this->blnUseHeader){
-	        $strHeader = $this->EvaluateTemplate(__VIEW_CORE_DIR__ . '/' . $this->strAssetMode . '/_header.inc.php');
+	        $strHeader = $this->EvaluateTemplate(__MJAX_CORE_VIEW__ . '/' . $this->strAssetMode . '/_header.inc.php');
 	        echo($strHeader);
     	}
     	
         require($this->strTemplate);
         
         if($this->blnUseHeader){
-	        $strFooter = $this->EvaluateTemplate(__VIEW_CORE_DIR__ . '/' . $this->strAssetMode . '/_footer.inc.php');
+	        $strFooter = $this->EvaluateTemplate(__MJAX_CORE_VIEW__ . '/' . $this->strAssetMode . '/_footer.inc.php');
 	        $strFormState = sprintf('<input type="hidden" name="%s" id="%s" value="%s" />', MJaxFormPostData::MJaxForm__FormState, MJaxFormPostData::MJaxForm__FormState, MJaxForm::Serialize($this));
 	        echo($strFormState);
 	        echo($strFooter);
@@ -648,6 +643,7 @@ class MJaxFormBase{
 		return(preg_match('/android.+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i',$strUseragent)||preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',substr($strUseragent,0,4)));
 	}
 	public static function DefineAssetDir($strAssetMode){
+		
 		if(!defined('__ASSETS__')){
 			define('__ASSETS__', __ASSETS_URL__ . '/' . $strAssetMode);
 			define('__ASSETS_JS__', __ASSETS__ . '/js');
--- package.inc.php
+++ package.inc.php
@@ -1,3 +1,59 @@
 <?php
-//require_once(dirname(__FILE__) . '/3.0/_core/MJax.inc.php');
-require_once(dirname(__FILE__) . '/3.1/_core/MJax.inc.php');
+ define("__MJAX__", dirname(dirname(__FILE__ ).'/MJax'));
+define("__MJAX_CORE__", __MJAX__ . "/_core");
+define("__MJAX_CORE_VIEW__", __MJAX_CORE__ . '/view');
+define("__MJAX_CORE_ASSET_URL__", MLCApplication::GetAssetUrl('', 'MJax'));
+		
+MLCApplicationBase::$arrClassFiles['MJaxApplication'] = __MJAX_CORE__ . '/MJaxApplication.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxFormBase'] = __MJAX_CORE__ . '/MJaxFormBase.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxForm'] = __MJAX_CORE__ . '/MJaxForm.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxControl'] = __MJAX_CORE__ . '/MJaxControl.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxControlBase'] = __MJAX_CORE__ . '/MJaxControlBase.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxCssClass'] = __MJAX_CORE__ . '/MJaxCssClass.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxControlStyle'] = __MJAX_CORE__ . '/MJaxControlStyle.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxTextBox'] = __MJAX_CORE__ . '/MJaxTextBox.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxButton'] = __MJAX_CORE__ . '/MJaxButton.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxImageButton'] = __MJAX_CORE__ . '/MJaxImageButton.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxListBox'] = __MJAX_CORE__ . '/MJaxListBox.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxListItem'] = __MJAX_CORE__ . '/MJaxListItem.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxPanel'] = __MJAX_CORE__ . '/MJaxPanel.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxAutoCompleteTextBox'] = __MJAX_CORE__ . '/MJaxAutoCompleteTextBox.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxDialogBox'] = __MJAX_CORE__ . '/MJaxDialogBox.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxNivoSlideShow'] = __MJAX_CORE__ . '/MJaxNivoSlideShow.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxJqFancySlideShow'] = __MJAX_CORE__ . '/MJaxJqFancySlideShow.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxLinkButton'] = __MJAX_CORE__ . '/MJaxLinkButton.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxUploadPanel'] = __MJAX_CORE__ . '/MJaxUploadPanel.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxControlProxy'] = __MJAX_CORE__ . '/MJaxControlProxy.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxCheckBox'] = __MJAX_CORE__ . '/MJaxCheckBox.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxFormStateHandler'] = __MJAX_CORE__ . '/MJaxFormStateHandler.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxTable'] = __MJAX_CORE__ . '/MJaxTable.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxTableRow'] = __MJAX_CORE__ . '/MJaxTableRow.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxRadioBox'] = __MJAX_CORE__ . '/MJaxRadioBox.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxUploadBox'] = __MJAX_CORE__ . '/MJaxUploadBox.class.php';
+
+
+//header_asset
+MLCApplicationBase::$arrClassFiles['MJaxHeaderAsset'] = __MJAX_CORE__ . '/header_assets/MJaxHeaderAsset.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxCssHeaderAsset'] = __MJAX_CORE__ . '/header_assets/MJaxCssHeaderAsset.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxJSHeaderAsset'] = __MJAX_CORE__ . '/header_assets/MJaxJSHeaderAsset.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxMetaHeaderAsset'] = __MJAX_CORE__ . '/header_assets/MJaxMetaHeaderAsset.class.php';
+//plugins
+MLCApplicationBase::$arrClassFiles['MJaxPluginBase'] = __MJAX_CORE__ . '/plugins/MJaxPluginBase.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxSelectMenuPlugin'] = __MJAX_CORE__ . '/plugins/MJaxSelectMenuPlugin.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxAnimatePlugin'] = __MJAX_CORE__ . '/plugins/MJaxAnimatePlugin.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxQTipPlugin'] = __MJAX_CORE__ . '/plugins/MJaxQTipPlugin.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxNivoSlideShowPlugin'] = __MJAX_CORE__ . '/plugins/MJaxNivoSlideShowPlugin.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxJqFancySlideShowPlugin'] = __MJAX_CORE__ . '/plugins/MJaxJqFancySlideShowPlugin.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxAutoCompletePlugin'] = __MJAX_CORE__ . '/plugins/MJaxAutoCompletePlugin.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxScrollToPlugin'] = __MJAX_CORE__ . '/plugins/MJaxScrollToPlugin.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxFancyboxPlugin'] = __MJAX_CORE__ . '/plugins/MJaxFancyboxPlugin.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxDatePickerPlugin'] = __MJAX_CORE__ . '/plugins/MJaxDatePickerPlugin.class.php';
+MLCApplicationBase::$arrClassFiles['MJaxLimitPlugin'] = __MJAX_CORE__ . '/plugins/MJaxLimitPlugin.class.php';
+//Extension
+MLCApplicationBase::$arrClassFiles['MJaxExtensionBase'] = __MJAX_CORE__ . '/extension/MJaxExtensionBase.class.php';
+
+require_once(__MJAX_CORE__ .  "/_enum.php");
+require_once(__MJAX_CORE__ .  "/_actions.php");
+require_once(__MJAX_CORE__ .  "/_events.php");
+require_once(__MJAX_CORE__ .  "/_functions.inc.php");
+require_once(__MJAX_CORE__ .  "/_exceptions.inc.php");
